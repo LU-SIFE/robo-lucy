4ed3aafe728af8bd7fdd4d616e85648508f445d6
LU SIFE <michaelleix@outlook.com>
2022-11-16 19:02:51 -0700

    re-added im back scenario

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -7,6 +7,16 @@ scan(message);
 
 function scan(msg) {
 
+	const triggerWords = ['im back', "i'm back", "I'm back", "IM BACK", "I'M BACK"];
+
+	for (i = 0; i < triggerWords.length; i++) {
+		if (msg.content.includes(triggerWords[i])) {
+			msg.reply("Hi back, I'm Robo-Lucy!~");
+			console.log('EXECUTED: DADJOKE');
+			return;
+		}
+	}
+
 	const args = msg.content.trim().split(/ +/g);
 	if((args[0].toLowerCase() == 'i\'m' || args[0].toLowerCase() == 'im')) {
 		msg.reply(`Hi ${args.slice(1).join(' ')}, I'm Robo-Lucy!~`);
