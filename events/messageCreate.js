5c99727c5ea3084baf6a8436a23761b0abcd5770
LU SIFE <michaelleix@outlook.com>
2022-11-16 18:58:50 -0700

    destroyed message parsing

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,36 +1,23 @@
 module.exports = {
 	name: 'messageCreate',
-	async execute(message) {
-
-const triggerWords = [['im back', "i'm back", "I'm back", "IM BACK", "I'M BACK"],
-	"the end is never",
-	["I'm dead", "i'm dead", "im dead", "Im dead", "im ded", "Im ded"]
-];
+	async execute(message, client) {
 
 if (message.author.bot) return;
 scan(message);
 
 function scan(msg) {
-	for (i = 0; i < triggerWords[0].length; i++) {
-		if (msg.content.includes(triggerWords[0][i])) {
-			msg.reply("Hi back, I'm Robo-Lucy!~");
-			console.log('EXECUTED: DADJOKE');
-			return;
-		}
-	}
 
-	if (msg.content.includes(triggerWords[1])) {
-		msg.reply("The end is never the end is nev- yes, we get it lol");
-		console.log('EXECUTED: THE END');
+	const args = msg.content.trim().split(/ +/g);
+	if((args[0].toLowerCase() == 'i\'m' || args[0].toLowerCase() == 'im')) {
+		msg.reply(`Hi ${args.slice(1).join(' ')}, I'm Robo-Lucy!~`);
+		console.log('EXECUTED: DADJOKE');
 		return;
 	}
 
-	for (i = 0; i < triggerWords[2].length; i++) {
-		if (msg.content.includes(triggerWords[2][i])) {
-			msg.reply(":skull:");
-			console.log('EXECUTED: DEAD');
-			return;
-		}
+	if (msg.content.includes("the end is never")) {
+		msg.reply("The end is never the end is nev- yes, we get it lol");
+		console.log('EXECUTED: THE END');
+		return;
 	}
 }
 // LYRIC BOT LYRIC BOT
